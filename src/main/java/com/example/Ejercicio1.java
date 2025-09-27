21dc3104d716d3cfffe820bca7b0227f6e385ada
Guelo708 <mguelo7@gmail.com>
2025-09-26 19:00:19 -0500

    Solución a ejercicio #1

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -2,7 +2,31 @@ package com.example;
 
 public class Ejercicio1 {
     public static void ejercicio1() {
-        System.out.println("Ejercicio 1");
-        // tu código aquí
+
+// Declarar variables
+        int x = 10;
+        int y = 5;
+// Operaciones aritméticas
+        int resultadoSuma = x + y;
+        int resultadoResta = x - y;
+        int resultadoMultiplicacion = x * y;
+        int resultadoDivision = x / y;
+        int resultadoModulo = x % y;
+
+// Imprimir resultados con mensajes descriptivos
+        System.out.println("Suma: " + x + " + " + y + " = " + resultadoSuma);
+        System.out.println("Resta: " + x + " - " + y + " = " + resultadoResta);
+        System.out.println("Multiplicación: " + x + " * " + y + " = " + resultadoMultiplicacion);
+        System.out.println("División entera: " + x + " / " + y + " = " + resultadoDivision);
+        System.out.println("Módulo (residuo): " + x + " % " + y + " = " + resultadoModulo);
+    
+
+  }
+
+    public static void main(String[] args) {
+        ejercicio1();
+
+       
+        
     }
 }
